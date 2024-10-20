75d4710d820d692f1f5a922f6228f81e99824117
Alfredo Ruiz <rruiz@tsspe.com>
2024-10-20 05:22:33 -0500

    Client entity is created

---
+++ POS.Domain/Entities/Client.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace POS.Domain.Entities
+{
+    [Table("CLIENTE")]
+    public class Client : BaseEntity
+    {
+        [Column("ID_CLIENTE"), Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public override uint Id { get; set; }
+        [Column("NOMBRE_COMPLETO")]
+        public string FullName { get; set; } = string.Empty;
+        [Column("NUMERO_CELULAR")]
+        public string? CellphoneNumber { get; set; }
+        [Column("NUMERO_CELULAR_BUSQUEDA")]
+        public string? CellphoneNumberSearch { get; set; }
+    }
+}
