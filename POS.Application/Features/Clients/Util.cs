467b9f6171f7a433f6281507bf5efdf2d4aa7c68
Alfredo Ruiz <rruiz@tsspe.com>
2024-10-20 07:03:12 -0500

    Null condition is added into StandarisizeCellphoneNumber method

--- POS.Application/Features/Clients/Util.cs
+++ POS.Application/Features/Clients/Util.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace POS.Application.Features.Clients
 {
@@ -11,6 +6,11 @@ namespace POS.Application.Features.Clients
     {
         public static string StandarisizeCellphoneNumber(string cellphoneNumber)
         {
+            if (string.IsNullOrWhiteSpace(cellphoneNumber))
+            {
+                return string.Empty;
+            }
+
             return Regex.Replace(cellphoneNumber.Replace("+51", ""), @"[\s\+\(\)\-]+", "");
         }
     }
