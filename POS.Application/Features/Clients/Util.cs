3d0d1645835a59f8b178dad9402b07f707d3a4bb
Alfredo Ruiz <rruiz@tsspe.com>
2024-10-20 06:36:27 -0500

    Util Client is created

---
+++ POS.Application/Features/Clients/Util.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace POS.Application.Features.Clients
+{
+    public class Util
+    {
+        public static string StandarisizeCellphoneNumber(string cellphoneNumber)
+        {
+            return Regex.Replace(cellphoneNumber.Replace("+51", ""), @"[\s\+\(\)\-]+", "");
+        }
+    }
+}
